e1f091c0d6e527a603cfb7a231b7f23e416e4bd6
Bill Garrett <garrett@astralfrontier.org>
2022-05-19 23:14:54 -0700

    Add IDs to some objects

--- index.ts
+++ index.ts
@@ -1,4 +1,9 @@
 /**
+ * Many objects have a unique ID, often a database key.
+ */
+export type ConstellationCardId<T> = string;
+
+/**
  * An IMAGE is a reference to an icon or background image.
  * This can either be a hard-coded symbolic name, or a URL.
  */
@@ -8,7 +13,20 @@ export type ConstellationCardImage = string | null | undefined;
  * A DECK is a collection of CARDS.
  */
 export interface ConstellationCardDeck {
+    /**
+     * The unique ID of this deck
+     */
+    id: ConstellationCardId<ConstellationCardDeck>;
+
+    /**
+     * The human readable name of this deck, e.g. "CORE"
+     */
     name: string;
+
+    /**
+     * All the cards in this deck, by ID
+     */
+    cards: ConstellationCardId<ConstellationCard>[];
 }
 
 /**
@@ -17,41 +35,78 @@ export interface ConstellationCardDeck {
  * Multiple DECKS can contribute cards to a single stack.
  */
 export interface ConstellationCardStack {
-    // Stacks have a name, e.g. "Character (Focus)"
+    /**
+     * The unique ID of this stack
+     */
+    id: ConstellationCardId<ConstellationCardStack>;
+
+    /**
+     * The human readable name of this stack, e.g. "Core Rules"
+     */
     name: string;
 
-    // Stacks have symbols attached to them
+    /**
+     * The list of icons associated with this stack
+     */
     icons: ConstellationCardImage[];
+
+    /**
+     * All the cards in this stack, by ID
+     */
+    cards: ConstellationCardId<ConstellationCard>[];
 }
 
 export interface ConstellationCardFace {
-    // Every face has a unique name; sometimes a card front and back faces share it
+    /**
+     * Every face has a unique name; sometimes a card front and back faces share it
+     */
     name: string;
 
-    // Cards can have a semi-transparent image as a background
+    /**
+     * Cards can have a semi-transparent image as a background
+     */
     backgroundImage: ConstellationCardImage;
 
-    // Cards can have descriptions
+    /**
+     * Cards can have descriptions
+     */
     description: string | undefined;
 
-    // Cards can optionally have a list of prompts, shown as a bulleted list
+    /**
+     * Cards can optionally have a list of prompts, shown as a bulleted list
+     */
     prompts: string[] | undefined;
 
-    // Cards can optionally have a rule at the bottom
+    /**
+     * Cards can optionally have a rule at the bottom
+     */
     rule: string | undefined;
 }
 
 export interface ConstellationCard {
-    // All cards belong in a deck, e.g. "CORE"
+    /**
+     * The unique ID of this card
+     */
+    id: ConstellationCardId<ConstellationCard>;
+
+    /**
+     * All cards belong in a deck, e.g. "CORE"
+     */
     deck: ConstellationCardDeck;
 
-    // All cards belong in a stack, e.g. "Character (Focus)"
+    /**
+     * All cards belong in a stack, e.g. "Character (Focus)"
+     */
     stack: ConstellationCardStack;
 
-    // Cards have front and back faces
+    /**
+     * Cards have front and back faces
+     */
     front: ConstellationCardFace;
     back: ConstellationCardFace;
 
-    // The stack has a certain number of this card in it
+    /**
+     * The stack has a certain number of this card in it
+     */
     quantity: number;
 }
